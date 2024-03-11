a0981819b9a4cf910eac6993e9bcfc7aa25bd85d
Deepika <deepikashar01@gmail.com>
2024-03-11 11:51:08 +0530

    Commit Mydb changes

---
+++ mydbqueries.py
@@ -0,0 +1,12 @@
+insert_query="insert into student values(106,'DEF')"
+update_qyuery="update student set sname = 'Deep' where sid=105"
+del_query="delete from student where sid =104"
+
+import mysql.connector
+
+con=mysql.connector.connect(host="localhost",port=3306,user="root",passwd="root",database="mydb")
+curs=con.cursor()
+curs.execute(insert_query)
+con.commit()
+con.close()
+print("Finished")
